24d42836d68879bbc29b1a6c822131823b8f6d65
Kai Jun Ang <angkaijun27@gmail.com>
2021-03-29 21:11:34 +0800

    Link brings to new tab for search

--- js/searchBar.js
+++ js/searchBar.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
    $.each(data, function(key, value){
     if (value.course_name.search(expression) != -1 || value.course_code.search(expression) != -1 || value.school.search(expression) != -1)
     {
-     $('#result').append('<li class="list-group-item link-class"><strong>'+value.course_name+'</strong> | <strong>'+value.course_code+'</strong> | <span class="text-muted"><strong>'+value.school+'</strong></span> | <p><em>'+value.course_description+'</em></p> | <a href = "'+value.reference+'">Click Here</a></li>');
+     $('#result').append('<li class="list-group-item link-class"><strong>'+value.course_name+'</strong> | <strong>'+value.course_code+'</strong> | <span class="text-muted"><strong>'+value.school+'</strong></span> | <p><em>'+value.course_description+'</em></p> | <a target="_blank" href = "'+value.reference+'">Click Here</a></li>');
     }
    });   
   });
